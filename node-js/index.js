1524cb8b135deeed4ab55fa98773583d0136726d
7vTr0x7 <7vtrox7@gmail.com>
2024-03-30 13:59:19 +0530

    Node.js server

--- node-js/index.js
+++ node-js/index.js
@@ -1,1 +1,15 @@
-console.log("hi");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/about") {
+    res.end("<h1>About</h1>");
+  } else if (req.url === "/") {
+    res.end("<h1>Home</h1>");
+  } else {
+    res.end("<h1>Not Found</h1>");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("server");
+});
