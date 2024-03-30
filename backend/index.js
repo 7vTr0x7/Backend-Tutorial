63c79cdd1326b703e91f91d9bb2ea23ec07a80a5
7vTr0x7 <7vtrox7@gmail.com>
2024-03-30 15:49:22 +0530

    ejs

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index", { name: "vtrox" });
 });
 
 app.listen(5000, () => {
