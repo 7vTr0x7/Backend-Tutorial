6ddb9a700a7ba3194d93daf5f59aaff101e40807
7vTr0x7 <7vtrox7@gmail.com>
2024-03-31 15:43:34 +0530

    ejs

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,20 @@ import express from "express";
 import path from "path";
 
 const app = express();
+const users = [];
 
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index", { name: "vtrox" });
 });
 app.post("/", (req, res) => {
   console.log(req.body);
+  users.push({ userName: req.body.name, email: req.body.email });
+  res.render("success");
 });
 
 app.listen(5000, () => {
