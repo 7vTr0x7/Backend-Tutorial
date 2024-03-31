85f901a2b62f83d51536ee421492a919768d4864
7vTr0x7 <7vtrox7@gmail.com>
2024-04-01 04:15:59 +0530

    mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import { name } from "ejs";
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017", {
+    dbName: "backend",
+  })
+  .then(() => console.log("connected"))
+  .catch((e) => console.log("Error", e));
 
 const app = express();
 const users = [];
+
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const Message = mongoose.model("Message", messageSchema);
 
 app.set("view engine", "ejs");
 
@@ -12,9 +28,13 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index", { name: "vtrox" });
 });
+
+app.get("/add", (req, res) => {
+  res.send("nice");
+});
+
 app.post("/contact", (req, res) => {
-  console.log(req.body);
-  users.push({ userName: req.body.name, email: req.body.email });
+  Message.create({ name: req.body.name, email: req.body.email });
   res.render("success");
 });
 
